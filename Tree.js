1422efdfdcda2d789c7515076d1991356dac0773
Austin Zensen <auze217@vt.edu>
2019-03-19 15:31:30 -0400

    starting treeclassvalue... we really need the trace

---
+++ Tree.js
@@ -0,0 +1,15 @@
+/**
+ * Class that creates a tree for a single visualization step
+ */
+class Tree{
+    /**
+     * 
+     * @param {Object} traceHeapForStep 
+     */
+    constructor(traceHeapForStep){
+        
+    }
+    
+    size() {
+    }
+}
---
+++ TreeClassValue.js
@@ -0,0 +1,16 @@
+/**
+ * class to represent the heap for tree class only
+ */
+class TreeClassValue{
+    /**
+     * 
+     * @param {reference} left 
+     * @param {reference} right 
+     * @param {Object} data 
+     */
+    constructor(left, right, data) {
+        this.TreeNodeLeft = null
+        this.TreeNodeRight = null
+
+    }
+}
